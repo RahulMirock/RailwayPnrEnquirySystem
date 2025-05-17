0b53b031dbf870e5d08438641e18bb76eb3b50ad
Rahul <166839670+RahulMirock@users.noreply.github.com>
2025-05-17 23:52:56 +0530

    Update GET.java

--- src/test/java/GET/GET.java
+++ src/test/java/GET/GET.java
@@ -11,6 +11,7 @@ public class GET {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Response r = given()
+			//now we have 3 things given,whenand then
 				.log().all()
 				
 				.when()
@@ -18,6 +19,7 @@ public class GET {
 				.get("http://localhost:3000/pnrStatus")
 				.then()
 				.log().all()
+			
 				
 			.statusCode(200)
 			.time(lessThan(500L))
